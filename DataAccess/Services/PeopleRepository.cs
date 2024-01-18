493a184a8835e0622698c0d433827909d10b13ae
niklas-hjelm <niklas.hjelm@iths.se>
2024-01-18 14:08:04 +0100

    update to use Atlas

--- DataAccess/Services/PeopleRepository.cs
+++ DataAccess/Services/PeopleRepository.cs
@@ -14,7 +14,8 @@ public class PeopleRepository
         var hostName = "localhost";
         var port = "27017";
         var databaseName = "PeopleDb";
-        var client = new MongoClient($"mongodb://{hostName}:{port}");
+        var connectionString = "mongodb+srv://testNiklas:apa123@cluster0.nviqhpj.mongodb.net/";//$"mongodb://{hostName}:{port}"
+        var client = new MongoClient(connectionString);
         var database = client.GetDatabase(databaseName);
         _people = database.GetCollection<Person>("People", new MongoCollectionSettings() { AssignIdOnInsert = true });
     }
